dc7942adb59ce75e3ef41e89a45c0485f4565f88
nOy39 <popov.a39rus@gmail.com>
2018-02-13 23:06:21 +0200

    MainControllers eddited, added Progress controller class, progress.fxml

--- src/main/java/fx/controllers/ProgressController.java
+++ src/main/java/fx/controllers/ProgressController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class Progress {
+public class ProgressController {
     @FXML
     private Button btnOk;
 
